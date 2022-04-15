a23e0839b06b3a8a773f7da5466116c6999c9a2e
mowuj <mowuj92@gmail.com>
2022-04-15 17:11:31 +0600

    project initialize and navbar

---
+++ src/components/Home/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { BsFillCartCheckFill } from "react-icons/bs";
+import logo2 from '../../../images/logo2.png'
+const Header = () => {
+    return (
+        <div>
+            <>
+
+  <Navbar bg="white" variant="light" >
+    <Container>
+        <Navbar.Brand as={Link} to="/">
+            <img height={30} src={logo2} alt="" />
+    </Navbar.Brand>
+    
+    
+    <Nav.Link className='ms-auto text-black' as={Link} to='/login'>
+        <BsFillCartCheckFill></BsFillCartCheckFill>
+    </Nav.Link>
+    <Nav.Link className='text-black' as={Link} to='/login'>
+        Login
+        </Nav.Link>
+        <button className='btn btn-link bg-info text-black text-decoration-none'>Signout</button>
+    </Container>
+  </Navbar>
+</>
+        </div>
+    );
+};
+
+export default Header;
